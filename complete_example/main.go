4f0ebb906aa1cc3100034494e92dd36e17b05341
Jeff Prestes <jeffprestes@gmail.com>
2020-06-17 20:39:35 -0300

    improve  msgs in portuguese

--- complete_example/main.go
+++ complete_example/main.go
@@ -4,7 +4,8 @@ import (
 	"bytes"
 	"crypto/ecdsa"
 	"encoding/hex"
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
@@ -16,56 +17,70 @@ func main() {
 	chavePrivadaEmHexadecimal := hex.EncodeToString([]byte(chavePrivada))
 	chavePrivadaEmECDSA, err := crypto.HexToECDSA(chavePrivadaEmHexadecimal)
 	if err != nil {
-		log.Fatal("Erro ao gerar a chave privada em ECDSA ", err)
+		fmt.Print("Erro ao gerar a chave privada em ECDSA ", err)
+		os.Exit(0)
 	}
 
 	chavePublica := chavePrivadaEmECDSA.Public()
 	chavePublicaEmECDSA, ok := chavePublica.(*ecdsa.PublicKey)
 	if !ok {
-		log.Fatal("Nao foi possivel fazer o casting da chave publica para ECDSA")
+		fmt.Print("Nao foi possivel fazer o casting da chave publica para ECDSA")
+		os.Exit(0)
 	}
 
-	log.Println("Temos as chaves...")
-	log.Println("Chave privada ", chavePrivada)
-	log.Println("Chave privada em Hexadecimal ", chavePrivadaEmHexadecimal)
-	log.Printf("Chave privada em ECDSA %+v\n", chavePrivadaEmECDSA)
-	log.Printf("Chave publica %+v\n", chavePublica)
-	log.Printf("Chave publica em ECDSA %+v\n", chavePublicaEmECDSA)
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+	fmt.Println("Temos a chave privada ", chavePrivada)
+	fmt.Println("Chave privada em Hexadecimal ", chavePrivadaEmHexadecimal)
+	fmt.Printf("\n")
+	fmt.Printf("Chave privada em ECDSA %+v\n", chavePrivadaEmECDSA)
+	fmt.Printf("\n")
+	fmt.Printf("Chave publica %+v\n", chavePublica)
+	fmt.Printf("\n")
+	fmt.Printf("Chave publica em ECDSA %+v\n", chavePublicaEmECDSA)
+	fmt.Printf("\n")
 
 	chavePublicaEmBytes := crypto.FromECDSAPub(chavePublicaEmECDSA)
 	chavePublicaEmHexaString := hexutil.Encode(chavePublicaEmBytes)
-	log.Printf("Chave publica em Texto [%x] - Len: %d\n", string(chavePublicaEmBytes), len(string(chavePublicaEmBytes)))
-	log.Println("Chave publica em Hexadecimal ", chavePublicaEmHexaString, " Len: ", len(chavePublicaEmHexaString), " O que e : ", chavePublicaEmBytes[0])
+	fmt.Printf("Chave publica em Texto [%x] - Len: %d\n", string(chavePublicaEmBytes), len(string(chavePublicaEmBytes)))
+	fmt.Printf("\n")
+	fmt.Println("Chave publica em Hexadecimal ", chavePublicaEmHexaString, " Len: ", len(chavePublicaEmHexaString), " O que e : ", chavePublicaEmBytes[0])
 
 	dado := "Eu vou assinar esse texto aqui"
 	hash := crypto.Keccak256Hash([]byte(dado))
-	log.Printf("Hash do dado a ser assinado %+v\n", hash.String())
-
+	fmt.Printf("\n")
+	fmt.Println("Texto a ser assinado: ", dado)
+	fmt.Printf("Hash do dado a ser assinado %+v\n", hash.String())
+	
 	assinatura, err := crypto.Sign(hash.Bytes(), chavePrivadaEmECDSA)
 	if err != nil {
-		log.Fatal("Erro ao assinar o dado ", err)
+		fmt.Print("Erro ao assinar o dado ", err)
+		os.Exit(0)
 	}
-	log.Printf("O dado assinado em hexa: %s\n", hexutil.Encode(assinatura))
+	fmt.Printf("O dado assinado em hexa: %s\n", hexutil.Encode(assinatura))
 
 	assinaturaChavePublica, err := crypto.Ecrecover(hash.Bytes(), assinatura)
 	if err != nil {
-		log.Fatalf("Erro ao gerar chave publica da assinatura %+v\n", err)
+		fmt.Printf("Erro ao gerar chave publica da assinatura %+v\n", err)
+		os.Exit(0)
 	}
 
+	fmt.Printf("\n")
 	ehValido := bytes.Equal(assinaturaChavePublica, chavePublicaEmBytes)
-	log.Println("A assinatura esta valida? ", ehValido)
+	fmt.Println("A assinatura esta valida? ", ehValido)
 
-	assinaturaChavePublicaECDSA, err := crypto.SigToPub(hash.Bytes(), assinatura)
-	if err != nil {
-		log.Fatalf("Erro ao gerar chave publica da assinatura em ECDSA %+v\n", err)
-	}
-
-	assinaturaChavePublicaECDSAEmBytes := crypto.FromECDSAPub(assinaturaChavePublicaECDSA)
-
-	ehValido = bytes.Equal(assinaturaChavePublicaECDSAEmBytes, chavePublicaEmBytes)
-	log.Println("A assinatura esta valida? ", ehValido)
+	// assinaturaChavePublicaECDSA, err := crypto.SigToPub(hash.Bytes(), assinatura)
+	// if err != nil {
+	// 	fmt.Printf("Erro ao gerar chave publica da assinatura em ECDSA %+v\n", err)
+	// 	os.Exit(0)
+	// }
+	// assinaturaChavePublicaECDSAEmBytes := crypto.FromECDSAPub(assinaturaChavePublicaECDSA)
+	// ehValido = bytes.Equal(assinaturaChavePublicaECDSAEmBytes, chavePublicaEmBytes)
+	// fmt.Println("A assinatura esta valida? ", ehValido)
 
 	assinaturaSemRecoverID := assinatura[:len(assinatura)-1]
 	verificado := crypto.VerifySignature(chavePublicaEmBytes, hash.Bytes(), assinaturaSemRecoverID)
-	log.Println("A assinatura esta verificada? ", verificado)
+	fmt.Println("A assinatura esta verificada? ", verificado)
+	fmt.Printf("\n")
+	fmt.Printf("\n")
 }
